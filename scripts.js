01ffa9bb09460c3bd939195dbe2a987571a2374b
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-07-31 15:35:46 -0400

    add UI and functions for determining the start of the game (player names / marks, current player) and move everything but making the actual board into a 'game' module

--- scripts.js
+++ scripts.js
@@ -1,11 +1,41 @@
 const gameBoard = document.getElementById('board')
 
-const Player = (name, mark) => {
-    return {name, mark}
+const player = (name, mark) => {
+    return {name,mark}
 }
 
+const makePlayer = player
+
+const game = (() => {
+    const winConditions = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,7],[2,5,8],[3,6,9]]
+    const checkCondition = ([a,b,c]) => {
+        if (board.spaces[a].value === player.mark && board.spaces[b].value === player.mark && board.spaces[c].value === player.mark) {
+            declareWinner(player)
+        }
+    }
+    const checkWin = () => {
+        winConditions.forEach(condition => 
+            checkCondition(condition))
+    }
+    let playerTwoIsBot = false
+    let playerOne = {}
+    let playerTwo = {}
+    let currentPlayer = {}
+    const start = () => {
+        const playerOneName = document.getElementById("player1-name").value
+        const playerOneMark = document.getElementById("player1-mark").value
+        const playerTwoName = playerTwoIsBot ? "The bot" : document.getElementById("player2-name").value
+        const playerTwoMark = (playerOneMark === "x") ? "o" : "x"
+        game.playerOne = player(playerOneName, playerOneMark)
+        game.playerTwo = player(playerTwoName, playerTwoMark)
+        game.currentPlayer = (Math.floor(Math.random()*2)===0) ? playerOne : playerTwo
+    }
+    document.getElementById("start-button").addEventListener("click",function() {start()})
+    return {checkWin, playerOne, playerTwo, currentPlayer}
+})()
+
 const board = (() => {
-    const boardObjects = []
+    const spaces = []
     for (let i=1; i<=9; i++) {
         const newSpace = document.createElement("div")
         newSpace.classList.add('space')
@@ -15,20 +45,10 @@ const board = (() => {
             name: `space-${i}`,
             value: ""
         }
-        boardObjects.push(newSpaceObject)
+        spaces.push(newSpaceObject)
 
     }
-    const winConditions = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,7],[2,5,8],[3,6,9]]
-    const checkCondition = ([a,b,c]) => {
-        if (boardObjects[a].value === player.mark && boardObjects[b].value === player.mark && boardObjects[c].value === player.mark) {
-            declareWinner(player)
-        }
-    }
-    const checkWin = () => {
-        winConditions.forEach(condition => 
-            checkCondition(condition))
-    }
-    return{boardObjects, checkWin}
+    return{spaces}
 })()
 
 
