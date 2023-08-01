9349ff7211f2712cf32a4195148780bfdc9f1a34
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-08-01 12:52:29 -0400

    add in functions for placing mark, which involves functions for checking win & stalemate conditions

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-const gameBoard = document.getElementById('board')
-const gameMessage = document.getElementById('display-message')
-
 const player = (name, mark) => {
     return {name,mark}
 }
@@ -8,38 +5,73 @@ const player = (name, mark) => {
 const makePlayer = player
 
 const game = (() => {
+    const gameMessage = document.getElementById('display-message')
     let playerTwoIsBot = false
     let playerOne
     let playerTwo
     let currentPlayer
+
     const start = () => {
         const playerOneName = document.getElementById("player1-name").value
         const playerOneMark = document.getElementById("player1-mark").value
         const playerTwoName = playerTwoIsBot ? "The bot" : document.getElementById("player2-name").value
         const playerTwoMark = (playerOneMark === "x") ? "o" : "x"
-        game.playerOne = player(playerOneName, playerOneMark)
-        game.playerTwo = player(playerTwoName, playerTwoMark)
+        playerOne = player(playerOneName, playerOneMark)
+        playerTwo = player(playerTwoName, playerTwoMark)
         const decider = Math.floor(Math.random()*2)
-        game.currentPlayer = (decider===0) ? game.playerOne : game.playerTwo
+        currentPlayer = (decider===0) ? playerOne : playerTwo
         document.getElementById("board").style.display = "grid"
         document.getElementById("starting-ui").style.display = "none"
-        gameMessage.textContent = `${game.currentPlayer.name}'s up first!`
+        gameMessage.textContent = `${currentPlayer.name}'s up first!`
     }
     document.getElementById("start-button").addEventListener("click",function() {start()})
 
-    
+    const placeMark = (element) => {
+        if (gameMessage.textContent === `${currentPlayer.name} wins!`) {return}
+        if (element.textContent !== "") {
+            gameMessage.textContent = `You can't take a space that's already taken, ${currentPlayer.name}!`
+            return
+        } 
+        element.textContent = `${currentPlayer.mark}`
+        board.spaces.forEach(object => {
+            if (object.name === element.id) {
+                object.mark = currentPlayer.mark
+            }
+        })
+        checkWin()
+        if (gameMessage.textContent === `${currentPlayer.name} wins!`) {return}
+        checkStalemate()
+        if (gameMessage.textContent === `Looks like it's a stalemate!`) {return}
+        currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne
+        gameMessage.textContent = `It's ${currentPlayer.name}'s turn`
+    }
 
-    const winConditions = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,7],[2,5,8],[3,6,9]]
+    const winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
     const checkCondition = ([a,b,c]) => {
-        if (board.spaces[a].value === game.currentPlayer.mark && board.spaces[b].value === game.currentPlayer.mark && board.spaces[c].value === game.currentPlayer.mark) {
-            declareWinner(player)
+        if (board.spaces[a].mark === currentPlayer.mark && board.spaces[b].mark === currentPlayer.mark && board.spaces[c].mark === currentPlayer.mark) {
+            gameMessage.textContent = `${currentPlayer.name} wins!`
+            console.log(`${currentPlayer.name} wins`)
+            return
         }
     }
     const checkWin = () => {
         winConditions.forEach(condition => 
             checkCondition(condition))
     }
-    return {checkWin, playerOne, playerTwo, currentPlayer}
+
+    const checkStalemate = () => {
+        let fullSpaces = 0
+        board.spaces.forEach(space => {
+            if (space.mark !== "") {
+                fullSpaces += 1
+            }
+        })
+        if (fullSpaces === 9) {
+            gameMessage.textContent = `Looks like it's a stalemate!`
+        }
+    }
+
+    return {placeMark}
 })()
 
 const board = (() => {
@@ -48,24 +80,13 @@ const board = (() => {
         const newSpace = document.createElement("div")
         newSpace.classList.add('space')
         newSpace.setAttribute("id",`space-${i}`)
-        gameBoard.appendChild(newSpace)
+        newSpace.addEventListener("click", function() {game.placeMark(this)})
+        document.getElementById('board').appendChild(newSpace)
         const newSpaceObject = {
             name: `space-${i}`,
-            value: ""
+            mark: ""
         }
         spaces.push(newSpaceObject)
-
     }
     return{spaces}
 })()
-
-
-
-// 1. Click occurs
-// 2. textContent of clicked cell becomes player's mark
-// 4. corresponding object in gameBoard array is marked with player's mark
-// 3. Game checks for wins
-//      8 win conditions checked by === operator checking array values,
-//      i.e. function that takes [(1, 2, 3)] -> 
-//      if (board[1].value === player.mark &&& board[1].value === player.mark &&& board[1].value === player.mark) display X winner
-//      
