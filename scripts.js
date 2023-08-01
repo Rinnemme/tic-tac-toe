17304b627f5fd85dfc52bbb893159a2ca0b7a7c8
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-08-01 17:43:33 -0400

    add bot functionality by implementing bot module and calling its functions conditionally if playerTwoIsBot

--- scripts.js
+++ scripts.js
@@ -23,14 +23,14 @@ const game = (() => {
     const gameMessage = document.getElementById('display-message')
     let playerTwoIsBot = false
     let gameOver = false
-    let playerOne
-    let playerTwo
-    let currentPlayer
+    let playerOne = {}
+    let playerTwo = {}
+    let currentPlayer = {}
 
     const start = () => {
         const playerOneName = document.getElementById("player1-name").value
         const playerOneMark = document.getElementById("player1-mark").value
-        const playerTwoName = playerTwoIsBot ? "The bot" : document.getElementById("player2-name").value
+        const playerTwoName = document.getElementById("player2-name").value
         const playerTwoMark = (playerOneMark === "x") ? "o" : "x"
         playerOne = player(playerOneName, playerOneMark)
         playerTwo = player(playerTwoName, playerTwoMark)
@@ -39,8 +39,25 @@ const game = (() => {
         document.getElementById("board").style.display = "grid"
         document.getElementById("starting-ui").style.display = "none"
         gameMessage.textContent = `${currentPlayer.name}'s up first!`
+        if (playerTwoIsBot && currentPlayer === playerTwo) {bot.takeTurn()}
     }
     document.getElementById("start-button").addEventListener("click", function() {start()})
+    
+    document.getElementById("bot-button").addEventListener("click", function() {
+        if (!playerTwoIsBot) {
+            playerTwoIsBot=true 
+            gameMessage.textContent = `Player two will be a bot`
+            document.getElementById("player2-name").value = "The bot"
+            document.getElementById("player2-label").textContent = "Bot's name"
+        } else {
+            playerTwoIsBot=false 
+            gameMessage.textContent = `Player two will no longer be a bot`
+            document.getElementById("player2-name").value = ""
+            document.getElementById("player2-label").textContent = "Player 2's Name"
+        }
+       
+       
+    })
 
     const winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
     const checkCondition = ([a,b,c]) => {
@@ -85,7 +102,68 @@ const game = (() => {
         if (gameOver) {return}
         currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne
         gameMessage.textContent = `It's ${currentPlayer.name}'s turn`
+        if (playerTwoIsBot && currentPlayer === playerTwo) {bot.takeTurn()}
     }
 
-    return {placeMark}
+    const getValues = () => {
+        return {playerOne, playerTwo, currentPlayer}
+    }
+
+    return {placeMark, getValues, winConditions}
+})()
+
+const bot = (() => {
+    const winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
+    const markWinnable = (() => {
+        winConditions.forEach(condition => {condition.winnable=true})
+    })()
+
+    const takeTurn = () => {
+        let turnFinished = false
+        winConditions.forEach(condition => {
+            condition.forEach(option => {
+                if (board.spaces[option].mark === game.getValues().playerOne.mark) {condition.winnable=false}
+            })
+        })
+        winConditions.filter(condition => condition.winnable)
+        // Add 'win if 2 of me in a row' before blocking
+        game.winConditions.forEach(condition => {
+            let counter = 0
+            condition.forEach(option => {
+                if (board.spaces[option].mark === game.getValues().playerOne.mark) {
+                    counter += 1
+                }
+            })
+            if (counter === 2) {
+                condition.forEach(option => {
+                    if (board.spaces[option].mark === "") {
+                        if (turnFinished) {return}
+                        game.placeMark(document.getElementById(`space-${option+1}`))
+                        turnFinished = true
+                    }
+                })
+            }
+        })
+        // Add 'place in a row where there's one of me before random placement on an open wincondition logic
+        if (turnFinished) {return}
+        const pickCondition = Math.floor(Math.random()*winConditions.length)
+        winConditions[pickCondition].forEach(option => {
+            if (board.spaces[option].mark === "") {
+                if (turnFinished) {return}
+                game.placeMark(document.getElementById(`space-${option+1}`))
+                turnFinished=true
+            }
+        })
+        if (!turnFinished) {
+            board.spaces.forEach(object => {
+                if (turnFinished) {return}
+                if (object.mark === "") {
+                game.placeMark(document.getElementById(`${object.name}`))
+                }
+                turnFinished = true
+            })
+        }
+    }
+
+    return {markWinnable, takeTurn}
 })()
