c5f484141ca0186fe0d86a557c6f603d8a71c1ad
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-08-02 14:38:13 -0400

    change message from using a conjunction for who's up first, add default player 1 and player 2 names for those respective players if fields are empty

--- scripts.js
+++ scripts.js
@@ -20,7 +20,7 @@ const board = (() => {
 })()
 
 const game = (() => {
-    const gameMessage = document.getElementById('display-message')
+    const message = document.getElementById('display-message')
     let playerTwoIsBot = false
     let gameOver = false
     let playerOne = {}
@@ -28,9 +28,9 @@ const game = (() => {
     let currentPlayer = {}
 
     const start = () => {
-        const playerOneName = document.getElementById("player1-name").value
+        const playerOneName = document.getElementById("player1-name").value === "" ? "Player 1" : document.getElementById("player1-name").value
+        const playerTwoName = document.getElementById("player2-name").value === "" ? "Player 2" : document.getElementById("player2-name").value
         const playerOneMark = document.getElementById("player1-mark").value
-        const playerTwoName = document.getElementById("player2-name").value
         const playerTwoMark = (playerOneMark === "x") ? "o" : "x"
         playerOne = player(playerOneName, playerOneMark)
         playerTwo = player(playerTwoName, playerTwoMark)
@@ -38,7 +38,7 @@ const game = (() => {
         currentPlayer = (firstTurnDecider === 0) ? playerOne : playerTwo
         document.getElementById("board").style.display = "grid"
         document.getElementById("starting-ui").style.display = "none"
-        gameMessage.textContent = `${currentPlayer.name}'s up first!`
+        message.textContent = `${currentPlayer.name} is up first!`
         if (playerTwoIsBot) {bot.getMarkValues()}
         if (playerTwoIsBot && currentPlayer === playerTwo) {bot.takeTurn()}
     }
@@ -49,13 +49,13 @@ const game = (() => {
         const playerTwoLabel = document.getElementById("player2-label")
         if (!playerTwoIsBot) {
             playerTwoIsBot=true 
-            gameMessage.textContent = `Player two will be a bot`
+            message.textContent = `Player two will be a bot`
             playerTwoLabel.textContent = "Bot's name"
             playerTwoInput.value = "The bot"
             playerTwoInput.setAttribute('readonly', true)
         } else {
             playerTwoIsBot=false 
-            gameMessage.textContent = `Player two will no longer be a bot`
+            message.textContent = `Player two will no longer be a bot`
             playerTwoLabel.textContent = "Player 2's Name"
             playerTwoInput.value = ""
             playerTwoInput.removeAttribute('readonly')
@@ -67,7 +67,7 @@ const game = (() => {
     const winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
     const checkCondition = ([a,b,c]) => {
         if (board.spaces[a].mark === currentPlayer.mark && board.spaces[b].mark === currentPlayer.mark && board.spaces[c].mark === currentPlayer.mark) {
-            gameMessage.textContent = `${currentPlayer.name} wins!`
+            message.textContent = `${currentPlayer.name} wins!`
             gameOver = true
             return
         }
@@ -85,7 +85,7 @@ const game = (() => {
             }
         })
         if (fullSpaces === 9) {
-            gameMessage.textContent = `Looks like it's a stalemate!`
+            message.textContent = `Looks like it's a stalemate!`
             gameOver = true
         }
     }
@@ -93,7 +93,7 @@ const game = (() => {
     const placeMark = (element) => {
         if (gameOver) {return}
         if (element.textContent !== "") {
-            gameMessage.textContent = `You can't take a space that's already taken, ${currentPlayer.name}!`
+            message.textContent = `You can't take a space that's already taken, ${currentPlayer.name}!`
             return
         } 
         element.textContent = `${currentPlayer.mark}`
@@ -106,7 +106,7 @@ const game = (() => {
         checkStalemate()
         if (gameOver) {return}
         currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne
-        gameMessage.textContent = `It's ${currentPlayer.name}'s turn`
+        message.textContent = `It's ${currentPlayer.name}'s turn`
         if (playerTwoIsBot && currentPlayer === playerTwo) {bot.takeTurn()}
     }
 
